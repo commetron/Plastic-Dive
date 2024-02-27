30d52261b7a52dc3ae15e5e65d53b3d00128099f
commetron <softechstar.pdf@outlook.com>
2024-02-27 18:32:58 +0200

    :boom: Allow more time on level 0

--- lib/constants.dart
+++ lib/constants.dart
@@ -13,7 +13,7 @@ class Constants {
   static const List<double> worldDeepness = [1000, 1500, 2000, 3000, 5000, 7500];
   static const List<double> maxDiverSpeed = [200, 250, 300, 350, 400, 500];
   static const List<double> collectSpeedFactor = [1, 1.2, 1.5, 2, 3, 5];
-  static const List<double> airTankCapacityInSeconds = [30, 60, 100, 150, 200, 250];
+  static const List<double> airTankCapacityInSeconds = [45, 60, 100, 150, 200, 250];
   static const double worldWidth = 5000;
   static const double worldWidthWithOffset = worldWidth + 2000;
 
